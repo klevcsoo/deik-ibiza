8e0407b078026b10e1b14137765b79a59ddf33e8
Keve Lajsz <kevelajsz@icloud.com>
2023-05-14 23:39:46 +0200

    Holnap be kell ezt mutatni

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from gyorshatvany import gyorshatvany
 from euklidesz import extended_gcd
 from miller_rabin import is_probable_prime
-from rsa import generate_rsa_keys, encrypt_rsa, decrypt_rsa
+from rsa import generate_rsa_keys, encrypt_rsa, decrypt_rsa, encrypt_rsa_text, decrypt_rsa_text
 
 if __name__ == "__main__":
     print("kibővített euklideszi algoritmus")
@@ -17,13 +17,21 @@ if __name__ == "__main__":
     print("\nmiller-rabin prímteszt")
     print(is_probable_prime(561))
 
-    print("\nrsa")
+    print("\nrsa (szám)")
     public_key, private_key = generate_rsa_keys(bit_length=1024)
     print(f"publikus kulcs (e, n): {public_key}")
     print(f"privát kulcs (d, n): {private_key}")
-    message = 42
-    print(f"eredeti üzenet: {message}")
-    ciphertext = encrypt_rsa(message, public_key)
+    original_number = 42
+    print(f"eredeti szám: {original_number}")
+    encrypted_number = encrypt_rsa(original_number, public_key)
+    print(f"titkosított szám: {encrypted_number}")
+    decrypted_number = decrypt_rsa(encrypted_number, private_key)
+    print(f"feloldott szám: {decrypted_number}")
+
+    print("\nrsa (szöveg)")
+    message = "elfogyott a xanax"
+    print(f"eredeti szám: {message}")
+    ciphertext = encrypt_rsa_text(message, public_key)
     print(f"titkosított üzenet: {ciphertext}")
-    decrypted_message = decrypt_rsa(ciphertext, private_key)
+    decrypted_message = decrypt_rsa_text(ciphertext, private_key)
     print(f"feloldott üzenet: {decrypted_message}")
--- rsa.py
+++ rsa.py
@@ -11,7 +11,7 @@ def generate_prime_number(bits):
             return n
 
 
-def generate_rsa_keys(bit_length):
+def generate_rsa_keys(bit_length: int):
     p = generate_prime_number(bit_length // 2)
     q = generate_prime_number(bit_length // 2)
 
@@ -31,21 +31,31 @@ def generate_rsa_keys(bit_length):
     return (e, n), (d, n)
 
 
-def encrypt_rsa(msg, pub_key):
-    e, n = pub_key
-    if msg < 0 or msg >= n:
-        raise ValueError("Message is out of range for encryption.")
-    return pow(msg, e, n)
+def encrypt_rsa(value: int, public_key: tuple[int, int]):
+    e, n = public_key
+    if value < 0 or value >= n:
+        raise ValueError("Túl nagy szám a titkosításhoz.")
+    return pow(value, e, n)
 
 
-def decrypt_rsa(cipher_text, priv_key):
-    d, n = priv_key
-    if cipher_text < 0 or cipher_text >= n:
-        raise ValueError("Ciphertext is out of range for decryption.")
-    return pow(cipher_text, d, n)
+def encrypt_rsa_text(text: str, public_key: tuple[int, int]):
+    char_values = [ord(c) for c in text]
+    return [encrypt_rsa(c, public_key) for c in char_values]
 
 
-def validate_rsa_keys(pub_key, priv_key):
-    e, n = pub_key
-    d, m = priv_key
+def decrypt_rsa(cipher_value: int, private_key: tuple[int, int]):
+    d, n = private_key
+    if cipher_value < 0 or cipher_value >= n:
+        raise ValueError("Túl nagy szám a visszafejtéshez.")
+    return pow(cipher_value, d, n)
+
+
+def decrypt_rsa_text(ciphertext: list[int], private_key: tuple[int, int]):
+    char_array = [chr(c) for c in [decrypt_rsa(c, private_key) for c in ciphertext]]
+    return "".join(char_array)
+
+
+def validate_rsa_keys(public_key: tuple[int, int], private_key: tuple[int, int]):
+    e, n = public_key
+    d, m = private_key
     return (e * d) % m == 1
