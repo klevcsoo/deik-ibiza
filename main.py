e09e5e8974e87a0146d25eb140a53850b3e3bb2b
Keve Lajsz <kevelajsz@icloud.com>
2023-05-14 23:45:08 +0200

    Add interakció

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from gyorshatvany import gyorshatvany
 from euklidesz import extended_gcd
+from gyorshatvany import gyorshatvany
 from miller_rabin import is_probable_prime
-from rsa import generate_rsa_keys, encrypt_rsa, decrypt_rsa, encrypt_rsa_text, decrypt_rsa_text
+from rsa import generate_rsa_keys, encrypt_rsa_text, decrypt_rsa_text
 
 if __name__ == "__main__":
     print("kibővített euklideszi algoritmus")
@@ -17,20 +17,12 @@ if __name__ == "__main__":
     print("\nmiller-rabin prímteszt")
     print(is_probable_prime(561))
 
-    print("\nrsa (szám)")
+    print("\nrsa (szöveg)")
+    message = input("kérek egy üzenetet: ")
     public_key, private_key = generate_rsa_keys(bit_length=1024)
     print(f"publikus kulcs (e, n): {public_key}")
     print(f"privát kulcs (d, n): {private_key}")
-    original_number = 42
-    print(f"eredeti szám: {original_number}")
-    encrypted_number = encrypt_rsa(original_number, public_key)
-    print(f"titkosított szám: {encrypted_number}")
-    decrypted_number = decrypt_rsa(encrypted_number, private_key)
-    print(f"feloldott szám: {decrypted_number}")
-
-    print("\nrsa (szöveg)")
-    message = "elfogyott a xanax"
-    print(f"eredeti szám: {message}")
+    print(f"eredeti üzenet: {message}")
     ciphertext = encrypt_rsa_text(message, public_key)
     print(f"titkosított üzenet: {ciphertext}")
     decrypted_message = decrypt_rsa_text(ciphertext, private_key)
